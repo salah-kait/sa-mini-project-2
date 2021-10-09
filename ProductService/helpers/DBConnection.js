98833a0ee5db74b4b787c614ac47bd9b45c1fabb
Nejed Hatamleh <najedhatamleh77@gmail.com>
2021-10-09 18:53:50 -0500

    Products service

---
+++ ProductService/helpers/DBConnection.js
@@ -0,0 +1,16 @@
+var knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  },
+   debug: true,
+   log: {
+    error(message) {
+    }
+  }
+});
+exports.knex =knex;
--- ProductService/index.js
+++ ProductService/index.js
@@ -1,16 +1,47 @@
 const express = require("express");
 const env = require("env2")(".env");
+const knex = require("./helpers/DBConnection").knex;
+const bodyParser = require("body-parser");
+const Helpers = require("./helpers/Helpers");
 
 const app = express();
+const port = process.env.PORT;
 
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT
-console.log(port);
+app.post('/products/new', (req, res) => {
+    knex('products').insert(req.body).then((data) => {
+        console.log(data);
+    });
+
+    res.sendStatus(200);
+});
+
+app.get('/products', (req, res) => {
+    knex("products").select().then((data) => {
+        res.send(data);
+    });  
+});
+
+app.post('/product-ordered', (req, res) => {
+    knex('products').select('quantity').where({ id: req.body.id }).first().then((data) => {
+        return knex('products')
+        .where({ id: req.body.id })
+        .update({
+          quantity: data.quantity - req.body.qnt
+        });
+
+        //TODO: Axios to the stock service if quantity belowe thrushold
+    });
+
+    res.send('Ok From Order Service')
+});
 
 app.get('/', (req, res) => {
-    res.send('Ok From Product Service')
-})
+  res.send('Ok From Product Service')
+});
 
 app.listen(port, () => {
-    console.log(`Product Service listening at http://localhost:${port}`)
-})
+  console.log(`Product Service listening at http://localhost:${port}`)
+});

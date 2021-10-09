1566d6c819348af11037885eaff1101370e59071
Nejed Hatamleh <najedhatamleh77@gmail.com>
2021-10-09 14:21:24 -0500

    Initial work Authentication service

--- AccontService/src/main/resources/data.sql
+++ AccontService/src/main/resources/data.sql
@@ -3,3 +3,11 @@ VALUES
 	(1, 'ROLE_ADMIN'),
 	(2, 'ROLE_PROVIDER'),
 	(3, 'ROLE_CLIENT');
+
+INSERT IGNORE INTO `users` (`id`, `name`, `username`, `password`, `email`)
+VALUES
+    (1, 'Test', 'TestName', '$2a$10$jN1DABY9UcjOc9LkC0619exAZ9.IClPnTb71hgrj3gmz6uobBM9Au', 'test@gmail.com');
+
+INSERT IGNORE INTO `user_roles` (`user_id`, `role_id`)
+VALUES
+    (1, 1);

4a34510310d8a8d6bb229917de610ba5c4309355
Nejed Hatamleh <najedhatamleh77@gmail.com>
2021-10-10 18:10:30 -0500

    Get user by ID for payment end-pint

--- AccontService/src/main/java/edu/miu/cs/cs590/accountservice/Controllers/PaymentController.java
+++ AccontService/src/main/java/edu/miu/cs/cs590/accountservice/Controllers/PaymentController.java
@@ -1,7 +1,11 @@
 package edu.miu.cs.cs590.accountservice.Controllers;
 
 import edu.miu.cs.cs590.accountservice.Models.Payment;
+import edu.miu.cs.cs590.accountservice.Security.CurrentUser;
+import edu.miu.cs.cs590.accountservice.Security.UserPrincipal;
 import edu.miu.cs.cs590.accountservice.Services.PaymentService;
+import edu.miu.cs.cs590.accountservice.Services.UserService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +22,12 @@ public class PaymentController {
     @Autowired
     PaymentService paymentService;
 
+    @Autowired
+    UserService userService;
+
     @PostMapping
-    public ResponseEntity<?> payment(@Valid @RequestBody Payment payment) {
+    public ResponseEntity<?> payment(@Valid @RequestBody Payment payment, @CurrentUser UserPrincipal userPrincipal) throws NotFoundException {
+        payment.setUser(userService.getUserById(userPrincipal.getId()));
         return ResponseEntity.ok(paymentService.save(payment));
     }
 }

9c31c36724684c5cdc7fe4d4f90814a9be50e6f1
Nejed Hatamleh <najedhatamleh77@gmail.com>
2021-10-09 22:18:07 -0500

    Stock Service

--- ProductService/index.js
+++ ProductService/index.js
@@ -2,44 +2,53 @@ const express = require("express");
 const env = require("env2")(".env");
 const knex = require("./helpers/DBConnection").knex;
 const bodyParser = require("body-parser");
-const Helpers = require("./helpers/Helpers");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT;
+const stockURL = process.env.STOCK_SERVICE_URL;
+const productMinQuantity = process.env.PRODUCT_LIMIT;
 
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/products/new', (req, res) => {
-    knex('products').insert(req.body).then((data) => {
-        console.log(data);
-    });
+  knex('products').insert(req.body).then((data) => {
+    console.log(data);
+  });
 
-    res.sendStatus(200);
+  res.sendStatus(200);
 });
 
 app.get('/products', (req, res) => {
-    knex("products").select().then((data) => {
-        res.send(data);
-    });  
+  knex("products").select().then((data) => {
+    res.send(data);
+  });
 });
 
 app.post('/product-ordered', (req, res) => {
-    knex('products').select('quantity').where({ id: req.body.id }).first().then((data) => {
-        return knex('products')
-        .where({ id: req.body.id })
-        .update({
-          quantity: data.quantity - req.body.qnt
-        });
+  let storedQuantity;
+  knex('products').select('quantity').where({ id: req.body.id }).first().then((data) => {
+    storedQuantity = data.quantity;
+      knex('products')
+      .where({ id: req.body.id })
+      .update({
+        quantity: storedQuantity - req.body.qnt
+      }).then(() => {
+        console.log(storedQuantity)
+        if (storedQuantity < productMinQuantity) {
+          axios.get(stockURL + '/stock-up').then((res) => {
+            console.log(res.data)
+          });
+        }
+      });
+  });
 
-        //TODO: Axios to the stock service if quantity belowe thrushold
-    });
-
-    res.send('Ok From Order Service')
+  res.send('Ok From Order Service')
 });
 
 app.get('/', (req, res) => {
-  res.send('Ok From Product Service')
+  res.status(200).send('Ok From Product Service')
 });
 
 app.listen(port, () => {
---
+++ StockService/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const env = require("env2")(".env");
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.get('/stock-up', (req, res) => {
+  console.log("stock up requested");
+  res.sendStatus(200);
+});
+
+app.get('/', (req, res) => {
+  res.status(200).send('Ok From Stock Service')
+});
+
+app.listen(port, () => {
+  console.log(`Stock Service listening at http://localhost:${port}`)
+});
